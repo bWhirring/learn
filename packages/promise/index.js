95077464e9fb2cf225524eb2c57a39999696f8d4
huhu <bianlongting@dianwoba.com>
2018-12-27 15:19:44 +0800

    fix

--- packages/promise/index.js
+++ packages/promise/index.js
@@ -25,6 +25,67 @@ class Promise {
       reject(e);
     }
   }
+
+  resolvePromise(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+      // 如果从onFulfilled中返回的x 就是promise2 就会导致循环引用报错
+      return reject(new TypeError("循环引用"));
+    }
+
+    let called = false; // 避免多次调用
+    // 如果x是一个promise对象 （该判断和下面 判断是不是thenable对象重复 所以可有可无）
+    if (x instanceof Promise) {
+      // 获得它的终值 继续resolve
+      if (x.status === PENDING) {
+        // 如果为等待态需等待直至 x 被执行或拒绝 并解析y值
+        x.then(
+          y => {
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          reason => {
+            reject(reason);
+          }
+        );
+      } else {
+        // 如果 x 已经处于执行态/拒绝态(值已经被解析为普通值)，用相同的值执行传递下去 promise
+        x.then(resolve, reject);
+      }
+      // 如果 x 为对象或者函数
+    } else if (
+      x != null &&
+      (typeof x === "object" || typeof x === "function")
+    ) {
+      try {
+        // 是否是thenable对象（具有then方法的对象/函数）
+        let then = x.then;
+        if (typeof then === "function") {
+          then.call(
+            x,
+            y => {
+              if (called) return;
+              called = true;
+              resolvePromise(promise2, y, resolve, reject);
+            },
+            reason => {
+              if (called) return;
+              called = true;
+              reject(reason);
+            }
+          );
+        } else {
+          // 说明是一个普通对象/函数
+          resolve(x);
+        }
+      } catch (e) {
+        if (called) return;
+        called = true;
+        reject(e);
+      }
+    } else {
+      resolve(x);
+    }
+  }
+
   then(onFulFilled, onRejected) {
     onFulFilled = typeof onFulFilled === "function" ? onFulFilled : y => y;
     onRejected =
@@ -40,7 +101,7 @@ class Promise {
           //“宏任务（macro-task）”机制实现
           try {
             let x = onFulFilled(this.value);
-            resolvePromise(promise2, x, resolve, reject);
+            this.resolvePromise(promise2, x, resolve, reject);
           } catch (e) {
             reject(e);
           }
@@ -53,7 +114,7 @@ class Promise {
           //“宏任务（macro-task）”机制实现
           try {
             let x = onRejected(this.reason);
-            resolvePromise(promise2, x, resolve, reject);
+            this.resolvePromise(promise2, x, resolve, reject);
           } catch (e) {
             reject(e);
           }
@@ -66,7 +127,7 @@ class Promise {
           setTimeout(() => {
             try {
               let x = onFulFilled(this.value);
-              resolvePromise(promise2, x, resolve, reject);
+              this.resolvePromise(promise2, x, resolve, reject);
             } catch (e) {
               reject(e);
             }
@@ -77,7 +138,7 @@ class Promise {
           setTimeout(() => {
             try {
               let x = onRejected(this.reason);
-              resolvePromise(promise2, x, resolve, reject);
+              this.resolvePromise(promise2, x, resolve, reject);
             } catch (e) {
               reject(e);
             }
