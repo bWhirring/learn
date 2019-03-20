25fb35c3092d12ac4684da9fca7da0c27631bd3a
huhu <bianlongting@dianwoda.com>
2019-03-20 13:49:19 +0800

    optimize

--- packages/acorn-demo/src/index.ts
+++ packages/acorn-demo/src/index.ts
@@ -1,10 +1,10 @@
-import { Parser, Options } from "acorn"
+import { Parser, Options } from "acorn";
 
 const defaultOptions: Options = {
-  ecmaVersion: 9,   // ECMAScript version to parse,
+  ecmaVersion: 9, // ECMAScript version to parse,
   sourceType: "module", // the mode the code should be parsed in., the influences global strice mode and parsing of `import` and `export` declarations
-  ranges: true,
-}
+  ranges: true
+};
 
 const code = `const test = () => console.log(1)`;
 
@@ -14,4 +14,4 @@ const token = Parser.tokenizer(code, defaultOptions);
 
 console.log(ast);
 
-console.log(token);
+// console.log(token);
